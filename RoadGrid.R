41771f14e8017594036ea7a8a691594396e87afe
Xavi Gimenez <xavi@xavigimenez.net>
2022-12-01 11:58:33 +0000

    document code

--- RoadGrid.R
+++ RoadGrid.R
@@ -8,7 +8,9 @@ library(osmdata)
 library(tidyverse)
 library(cowplot)
 
+
 #----Fill out this section with your info before running the rest of the code----
+
 #Pick a city to plot
 city <- 'Portland, OR'
 
@@ -20,28 +22,56 @@ plotcolors <- c('#59C8E5', '#0A7ABF', '#2E968C', '#4CB580',  '#FED032','#FF9223'
 
 
 #---get the road network from OSM----
+
+# get bounding box for a given place name
 bb <- getbb(city)
+
+# build an overpass query
 qry <- opq(bbox = bb, timeout = 300) %>%
+  # add the ley feature 'highway' to the overpass query
   add_osm_feature(key = 'highway') %>%
+  # get overpass query as an osmdata object in sf format
+  # sf (simple features format: https://en.wikipedia.org/wiki/Simple_Features
+  # how sf in R are organized: https://r-spatial.github.io/sf/articles/sf1.html#how-simple-features-in-r-are-organized
+  # sf vignette: https://cran.r-project.org/web/packages/sf/sf.pdf
   osmdata_sf()
+
+# getting a Simple Features collection of linestrings
 roads <- qry$osm_lines %>% select(geometry, name)
 
+
 #----find road bearings----
+
+# initialize the dataframe that contains geo data
 bearings <- data.frame(b = numeric(), l = numeric(), name = character())
+
 for (i in 1:(length(roads$geometry))){
+  #geometry contains linestring like:
+  # LINESTRING (-122.7928 45.52251, -122.7927 45.52237, -122.7927 45.52214, -122.7926 45.52201, -122.7925 45.52189, -122.7922 45.52141, -122.7919 45.52085, -122.7918 45.52074, -122.7916 45.52046, -122.7916 45.52038, -122.7915 45.52024, -122.7914 45.52006, -122.7914 45.51997, -122.7914 45.51985, -122.7914 45.51974)
+  # length of linestring above is 30, somehow it counts every single value although the linestring is composed of <lon,lat> tuples
   k <- length(roads$geometry[[i]])/2
+  # get first and last pair of <lon,lat> coordinates
   lon1 <- roads$geometry[[i]][1,1]
-  lon2 <- roads$geometry[[i]][k,1]
   lat1 <- roads$geometry[[i]][1,2]
+  lon2 <- roads$geometry[[i]][k,1]
   lat2 <- roads$geometry[[i]][k,2]
   
-  bearings <- bind_rows(bearings, 
-                        data.frame(b = geosphere::bearing(c(lon1, lat1),c(lon2, lat2)),
-                                   l = geosphere::distHaversine(c(lon1, lat1),c(lon2, lat2)),
-                                   name = roads$name[i]))
+  bearings <- bind_rows(
+    bearings, 
+    data.frame(
+      # get direction in degrees for each linestring
+      b = geosphere::bearing(c(lon1, lat1),c(lon2, lat2)),
+      # shortest distance between first and last points of linestring
+      l = geosphere::distHaversine(c(lon1, lat1),c(lon2, lat2)),
+      # keep the name of the linestring, if any
+      name = roads$name[i]
+    )
+  )
 }
 
+
 #----get ready to plot----
+
 #get the inverse of each bearing (e.g. if a road goes 90deg, it also goes at 270deg)
 plot <- bind_rows(bearings,
                   data.frame(b = bearings$b[bearings$b<=180]+180, l = bearings$l[bearings$b<=180], name = bearings$name[bearings$b<=180])) %>%
@@ -63,7 +93,7 @@ for (i in 1:length(plottype)) {
   plotme <- subset(plot, grepl(paste0(" ", plottype[i], "$"), name))
   color <- plotcolors[i]
   
-  #firt get the basic plot ready
+  #first get the basic plot ready
   basicplot <- ggplot(plotme, aes(x = b, weight = l)) +
     geom_histogram(binwidth = 10,  boundary = 5, size = .01, closed = 'left',
                    fill = color, color = 'black', aes(y=..count../sum(..count..))) +
